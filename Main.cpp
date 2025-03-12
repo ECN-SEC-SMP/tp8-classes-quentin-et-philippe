bafe5b4cb62c00bd6f738befa714345c18dfc590
simbadLM <quentin.balloy.pro@gmail.com>
2025-03-12 19:00:35 +0100

    [WIP] Ajout dans Main.cpp de methode deplacement + procedure game

--- Main.cpp
+++ Main.cpp
@@ -2,21 +2,29 @@
 #include <cstdlib>
 #include <ctime>
 
+#include "Lion.h"
+#include "Loup.h"
+#include "Ours.h"
+
 using namespace std;
 
+/* ---------------------INIT PREALABLES AU JEU ---------------------- */
+int cptTour = 0;
+bool jeuPossible = true;
+char actionJoueur = ' ';
 const int maxX = 10;  
 const int maxY = 10; 
 
 // Fonction qui génère un tableau de taille maxX * maxY et le remplit
-char** genererBoard() {
+string** genererBoard() {
     srand(time(0)); // Initialisation du générateur aléatoire
 
-    char v_animal[] = {'W', 'P', 'O', 'L'};
+    std::string v_animal[] = {"W", "S", "B", "L"};
 
     // Allocation dynamique du tableau 2D
-    char** board = new char*[maxX];
+    std::string** board = new std::string*[maxX];
     for (int i = 0; i < maxX; ++i) {
-        board[i] = new char[maxY];
+        board[i] = new std::string[maxY];
     }
 
     int totalCells = maxX * maxY;
@@ -25,7 +33,7 @@ char** genererBoard() {
     // Initialisation du tableau vide
     for (int i = 0; i < maxX; ++i) {
         for (int j = 0; j < maxY; ++j) {
-            board[i][j] = ' ';  // Cellule vide
+            board[i][j] = " ";  // Cellule vide
         }
     }
 
@@ -34,8 +42,8 @@ char** genererBoard() {
         int r = rand() % maxX;
         int c = rand() % maxY;
 
-        // S'assurer que la cellule n'est pas déjà remplie
-        while (board[r][c] != ' ') {
+        // S"assurer que la cellule n"est pas déjà remplie
+        while (board[r][c] != " ") {
             r = rand() % maxX;
             c = rand() % maxY;
         }
@@ -45,8 +53,10 @@ char** genererBoard() {
     return board;
 }
 
+
 // Fonction qui affiche le tableau
-void afficherBoard(char** i_board) {
+void afficherBoard(std::string** i_board) {
+    int nombreAnimauxVivants = 0;
     // Ligne supérieure
     for (int j = 0; j < maxY; ++j) {
         cout << "|----";
@@ -56,6 +66,7 @@ void afficherBoard(char** i_board) {
     for (int i = 0; i < maxX; ++i) {
         for (int j = 0; j < maxY; ++j) {
             // Afficher la cellule avec sa bordure
+            if (i_board[i][j] != " ") nombreAnimauxVivants++;
             cout << "| " << i_board[i][j] << "  ";
         }
         cout << "|" << endl;
@@ -66,11 +77,176 @@ void afficherBoard(char** i_board) {
         }
         cout << "|" << endl;
     }
+    cout << "Nombre d'animaux vivants : " << nombreAnimauxVivants << endl;
+}
+/* ------------------------------------------------ */
+
+/* ---------------------FONCTIONS DE JEU ---------------------- */
+void deplacementAnimaux(string **i_board) {
+    string cellule;
+    for (int ligne = 0; ligne < maxX; ligne++) {
+        for (int col = 0; col < maxY; col++) {
+
+            cellule = i_board[ligne][col];
+
+            if (i_board[ligne][col] != " ") {
+                for (int chaqueAnimal = 0; chaqueAnimal < size(cellule); chaqueAnimal++) {
+
+                    switch (cellule[chaqueAnimal]) {
+
+                        // "Pierre - Stone"
+                        case 'S': {
+                            //Un pierre ne peut se deplacer... Bien que chacun le sait : pierre qui **roule** n'amasse pas mousse...
+                            break;
+                        }
+
+                        // "Loup - Wolf"
+                        case 'W': {
+
+                            Loup* currentLoup = new Loup(ligne, col); // Instanciation d'un Loup
+                            currentLoup->deplace(ligne, col); // deplace par rapport a l'origine
+
+                            if (i_board[currentLoup->getX()][currentLoup->getY()] = ' ') i_board[currentLoup->getX()][currentLoup->getY()] = 'L'; // La case etait vide, on remplace le carac vide par L
+                            else i_board[currentLoup->getX()][currentLoup->getY()]+= 'L'; // La case n'est pas vide, on ajoute a la fin de la chaine
+                             
+                            break;
+                        }
+
+                        // "Lion - Lion "
+                        case 'L': {
+
+                            Lion* currentLion = new Lion(ligne, col); // Instanciation d'un Lion
+                            currentLion->deplace(ligne, col); // deplace par rapport a l'origine
+
+                            int XCorrige = 0;
+                            int YCorrige = 0;
+                    
+                            //TODO : Gérer le cas de débord
+
+                            if (currentLion->getX() >= maxX) XCorrige = currentLion->getX() - maxX; //Traite le cas X > taille de plateau
+                            if (currentLion->getX() <= maxX) XCorrige =  maxX - currentLion->getX(); //Traite le cas X < taille de plateau
+                            
+                            if (currentLion->getY() >= maxY) YCorrige = currentLion->getY() - maxY; //Traite le cas Y > taille de plateau
+                            if (currentLion->getY() <= maxY) YCorrige =  maxY - currentLion->getY(); //Traite le cas Y < taille de plateau
+                            
+                            
+
+                            if (XCorrige != 0) {
+
+                                if (YCorrige != 0) { 
+                                    // Pas de correction en X ni en Y
+                                    if (i_board[currentLion->getX()][currentLion->getY()] = ' ') i_board[currentLion->getX()][currentLion->getY()] = 'L'; // La case etait vide, on remplace le carac vide par L
+                                    else i_board[currentLion->getX()][currentLion->getY()]+= 'L'; // La case n'est pas vide, on ajoute a la fin de la chaine
+                                } 
+                                else {
+                                    // Correction uniquement en Y 
+                                    if (i_board[currentLion->getX()][YCorrige] = ' ') i_board[currentLion->getX()][YCorrige->getY()] = 'L'; // La case etait vide, on remplace le carac vide par L
+                                    else i_board[currentLion->getX()][YCorrige]+= 'L'; // La case n'est pas vide, on ajoute a la fin de la chaine
+                                }
+                            }
+
+                            else {
+
+                                if (YCorrige != 0) { 
+                                    // Correction uniquement en X
+                                    if (i_board[XCorrige][currentLion->getY()] = ' ') i_board[XCorrige][currentLion->getY()] = 'L'; // La case etait vide, on remplace le carac vide par L
+                                    else i_board[XCorrige][currentLion->getY()]+= 'L'; // La case n'est pas vide, on ajoute a la fin de la chaine
+                                } 
+                                else {
+                                    // Correction en X et en Y 
+                                    if (i_board[XCorrige][YCorrige] = ' ') i_board[XCorrige][YCorrige] = 'L'; // La case etait vide, on remplace le carac vide par L
+                                    else i_board[XCorrige][YCorrige]+= 'L'; // La case n'est pas vide, on ajoute a la fin de la chaine
+                                }
+                            }  
+
+                            break;
+                        }
+
+                        // "Ours - Ours "
+                        case 'B': {
+
+                            Ours* currentOurs = new Ours(ligne, col); // Instanciation d'un Ours
+                            currentOurs->deplace(ligne, col); // deplace par rapport a l'origine
+
+                            int XCorrige = 0;
+                            int YCorrige = 0;
+                    
+                            //TODO : Gérer le cas de débord
+
+                            if (currentOurs->getX() >= maxX) XCorrige = currentOurs->getX() - maxX; //Traite le cas X > taille de plateau
+                            if (currentOurs->getX() <= maxX) XCorrige =  maxX - currentOurs->getX(); //Traite le cas X < taille de plateau
+                            
+                            if (currentOurs->getY() >= maxY) YCorrige = currentOurs->getY() - maxY; //Traite le cas Y > taille de plateau
+                            if (currentOurs->getY() <= maxY) YCorrige =  maxY - currentOurs->getY(); //Traite le cas Y < taille de plateau
+                            
+                            
+
+                            if (XCorrige != 0) {
+
+                                if (YCorrige != 0) { 
+                                    // Pas de correction en X ni en Y
+                                    if (i_board[currentOurs->getX()][currentOurs->getY()] = ' ') i_board[currentOurs->getX()][currentOurs->getY()] = 'L'; // La case etait vide, on remplace le carac vide par L
+                                    else i_board[currentOurs->getX()][currentOurs->getY()]+= 'L'; // La case n'est pas vide, on ajoute a la fin de la chaine
+                                } 
+                                else {
+                                    // Correction uniquement en Y 
+                                    if (i_board[currentOurs->getX()][YCorrige] = ' ') i_board[currentOurs->getX()][YCorrige->getY()] = 'L'; // La case etait vide, on remplace le carac vide par L
+                                    else i_board[currentOurs->getX()][YCorrige]+= 'L'; // La case n'est pas vide, on ajoute a la fin de la chaine
+                                }
+                            }
+
+                            else {
+
+                                if (YCorrige != 0) { 
+                                    // Correction uniquement en X
+                                    if (i_board[XCorrige][currentOurs->getY()] = ' ') i_board[XCorrige][currentOurs->getY()] = 'L'; // La case etait vide, on remplace le carac vide par L
+                                    else i_board[XCorrige][currentOurs->getY()]+= 'L'; // La case n'est pas vide, on ajoute a la fin de la chaine
+                                } 
+                                else {
+                                    // Correction en X et en Y 
+                                    if (i_board[XCorrige][YCorrige] = ' ') i_board[XCorrige][YCorrige] = 'L'; // La case etait vide, on remplace le carac vide par L
+                                    else i_board[XCorrige][YCorrige]+= 'L'; // La case n'est pas vide, on ajoute a la fin de la chaine
+                                }
+                            }  
+                            break;
+
+                        
+
+                    }
+                    if (cellule[chaqueAnimal] != 'S') cellule.erase(chaqueAnimal, 1); // Supprime l'animal de la case actuelle sauf si c'est une pierre
+                }
+            }
+        }
+    }
+}
+
+void jeu() {
+
+    if (jeuPossible) {
+        cout << "--- Appuyez sur la touche Y puis Enter pour continuer, sur N pour arreter le jeu. ---" << endl;
+
+        cin >> actionJoueur;
+
+        if (actionJoueur == 'Y' || actionJoueur == 'y') {
+            cptTour++;
+            deplacementAnimaux();
+            roundAttaque();
+        } else if (actionJoueur == 'N' || actionJoueur == 'n') {
+            jeuPossible = false;
+            game();
+        }
+    } else {
+        cout << "Merci d'avoir jouer. Relancez pour une nouvelle partie" << endl;
+        jeuPossible = false;
+
+        return;
+    }
+    
 }
 
 int main() {
 
-    char** board = genererBoard();
+    std::string** board = genererBoard();
     afficherBoard(board);
 
     // Libérer la mémoire allouée
