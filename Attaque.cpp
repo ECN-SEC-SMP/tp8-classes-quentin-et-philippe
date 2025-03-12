21920acdcd84d44ef9cb3fe1b09eb974b1acbcf4
simbadLM <quentin.balloy.pro@gmail.com>
2025-03-12 12:08:34 +0100

    Ajout des fichiers Attaque.h & Attaque.cpp

---
+++ Attaque.cpp
@@ -0,0 +1,119 @@
+#include "Attaque.h"
+#include <_stdlib.h>
+#include <string>
+
+
+class Attaque {
+
+    enum eType {PIERRE = 0, FEUILLE = 1, CISEAUX = 2
+    };
+
+// Membres :
+
+    private : eType m_type;
+//---- 
+
+// Constructeurs :
+
+    public : 
+        Attaque::Attaque() {this->m_type = static_cast<eType>(rand() % 3 + 1);}; // Génère une attaque aleatoire
+        Attaque::Attaque(eType i_type) {this->m_type = i_type;};
+//----
+
+// Getters
+
+        eType getTypeAttaque() const {
+            return this->m_type;
+        }
+
+        std::string getNomAttaque() const {
+
+            switch (this->m_type) {
+
+                case PIERRE : {
+                    return "Pierre";
+                }
+
+                case FEUILLE : {
+                    return "Feuille";
+                    break;
+                }
+
+                case CISEAUX : {
+                    return "Ciseaux";
+                    break;
+                }
+            }
+        }
+//----
+
+// Methodes specifiques
+        
+        bool resoudreAttaque(Attaque &a) const {
+            
+            switch (this->m_type) {
+
+                case PIERRE : {
+
+                    switch (a.m_type) {
+
+                        case PIERRE : {
+                            // Match nul
+                            while (a.m_type == this->m_type) a.m_type = static_cast<eType>(rand() % 3 + 1);
+                            resoudreAttaque(a);
+                        }
+        
+                        case FEUILLE : {
+                            return false;
+                        }
+        
+                        case CISEAUX : {
+                            return true;
+                        }
+                    }
+                }
+
+                case FEUILLE : {
+                    switch (a.m_type) {
+
+                        case PIERRE : {
+                            return true;
+                        }
+        
+                        case FEUILLE : {
+                            // Match nul
+                            while (a.m_type == this->m_type) a.m_type = static_cast<eType>(rand() % 3 + 1);
+                            resoudreAttaque(a);
+                        }
+        
+                        case CISEAUX : {
+                            return false;
+                        }
+                    }
+                }
+
+                case CISEAUX : {
+                    switch (a.m_type) {
+
+                        case PIERRE : {
+                            return false;
+                        }
+        
+                        case FEUILLE : {
+                            return true;
+                        }
+        
+                        case CISEAUX : {
+                            // Match nul
+                            while (a.m_type == this->m_type) a.m_type = static_cast<eType>(rand() % 3 + 1);
+                            resoudreAttaque(a);
+                        }
+                    }
+                }
+            }
+        }
+//----
+
+};
+
+
